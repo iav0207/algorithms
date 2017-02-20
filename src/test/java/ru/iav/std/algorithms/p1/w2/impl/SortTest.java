614ecb4a5e957dadcf1b5e0bd0b8c9bd6461d45e
takoe <IsaevAV27@gmail.com>
2017-02-20 22:23:35 +0300

    Refactoring

--- src/test/java/ru/iav/std/algorithms/p1/w2/impl/SortTest.java
+++ src/test/java/ru/iav/std/algorithms/p1/w2/impl/SortTest.java
@@ -1,14 +1,9 @@
 package ru.iav.std.algorithms.p1.w2.impl;
 
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.iav.std.algorithms.p1.w2.Sort;
 
 import java.util.List;
-import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 import static org.testng.Assert.assertTrue;
 
@@ -17,20 +12,14 @@ import static org.testng.Assert.assertTrue;
  */
 public abstract class SortTest {
 
-    private static Random random = ThreadLocalRandom.current();
-
     private static final int invocations = 1;
 
-    private static final int items = 10_000;
-
-    protected abstract Sort sort();
-
-    @Test(dataProvider = "random", invocationCount = invocations)
+    @Test(dataProviderClass = SortTestData.class, dataProvider = "random", invocationCount = invocations)
     public void shouldSortRandomInput(List<Comparable<?>> list, Object ignore) throws Exception {
         assertSorted(list);
     }
 
-    @Test(dataProvider = "sorted", invocationCount = invocations)
+    @Test(dataProviderClass = SortTestData.class, dataProvider = "sorted", invocationCount = invocations)
     public void shouldSortSortedInput(List<Comparable<?>> list, Object ignore) throws Exception {
         assertSorted(list);
     }
@@ -41,6 +30,8 @@ public abstract class SortTest {
         assertTrue(isSorted(a));
     }
 
+    protected abstract Sort sort();
+
     private boolean isSorted(Comparable[] a) {
         if (a == null) return false;
         if (a.length < 2) return true;
@@ -48,30 +39,6 @@ public abstract class SortTest {
             if (a[i].compareTo(a[i-1]) < 0) return false;
         }
         return true;
-    }
-
-    @DataProvider(name = "random")
-    public static Object[][] random() {
-        return new Object[][]{
-                {
-                        random.ints(items)
-                                .boxed()
-                                .collect(Collectors.toList()),
-                        null
-                }
-        };
-    }
-
-    @DataProvider(name = "sorted")
-    public static Object[][] sorted() {
-        return new Object[][] {
-                {
-                        IntStream.range(0, items)
-                                .boxed()
-                                .collect(Collectors.toList()),
-                        null
-                }
-        };
     }
 
 }
---
+++ src/test/java/ru/iav/std/algorithms/p1/w2/impl/SortTestData.java
@@ -0,0 +1,43 @@
+package ru.iav.std.algorithms.p1.w2.impl;
+
+import org.testng.annotations.DataProvider;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * Created by takoe on 20.02.17.
+ */
+public class SortTestData {
+
+    private static Random random = ThreadLocalRandom.current();
+
+    private static final int items = 10_000;
+
+    @DataProvider(name = "random")
+    public static Object[][] random() {
+        return new Object[][]{
+                {
+                        random.ints(items)
+                                .boxed()
+                                .collect(Collectors.toList()),
+                        null
+                }
+        };
+    }
+
+    @DataProvider(name = "sorted")
+    public static Object[][] sorted() {
+        return new Object[][] {
+                {
+                        IntStream.range(0, items)
+                                .boxed()
+                                .collect(Collectors.toList()),
+                        null
+                }
+        };
+    }
+
+}
