12407d8be29d0f721ef1c22a1ebf3c25fc64907a
takoe <IsaevAV27@gmail.com>
2017-02-24 20:17:21 +0300

    Small refactoring

--- src/main/java/ru/iav/std/algorithms/p1/w3/sort/QuickWithInsertionSort.java
+++ src/main/java/ru/iav/std/algorithms/p1/w3/sort/QuickWithInsertionSort.java
@@ -7,12 +7,14 @@ import ru.iav.std.algorithms.p1.w2.sort.InsertionSort;
  */
 public class QuickWithInsertionSort extends QuickSort {
 
+    public static int CUTOFF = 10;
+
     private InsertionSort insertion = new InsertionSort();
 
     @Override
     protected void sort(Comparable[] a, int lo, int hi) {
         if (hi <= lo) return;
-        if (hi - lo < 10)
+        if (hi - lo < CUTOFF)
             insertion.sort(a, lo, hi + 1);
         else
             super.sort(a, lo, hi);
