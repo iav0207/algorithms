ef9aa90c44987474fcd9ef4a49b7dd43c9d3bc65
takoe <IsaevAV27@gmail.com>
2017-02-24 19:47:44 +0300

    Implement QuickSort

---
+++ src/main/java/ru/iav/std/algorithms/p1/w3/sort/QuickSort.java
@@ -0,0 +1,40 @@
+package ru.iav.std.algorithms.p1.w3.sort;
+
+import edu.princeton.cs.algs4.StdRandom;
+import ru.iav.std.algorithms.p1.w2.sort.AbstractSort;
+
+/**
+ * Created by takoe on 24.02.17.
+ */
+public class QuickSort extends AbstractSort {
+
+    @Override
+    protected void executeSorting(Comparable[] a) {
+        StdRandom.shuffle(a);
+        sort(a, 0, a.length - 1);
+    }
+
+    private void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) return;
+        int j = partition(a, lo, hi);
+        sort(a, lo, j - 1);
+        sort(a, j + 1, hi);
+    }
+
+    private int partition(Comparable[] a, int lo, int hi) {
+        int i = lo;
+        int j = hi + 1;
+        while (true) {
+            while (less(a[++i], a[lo])) {
+                if (i == hi) break;
+            }
+            while (less(a[lo], a[--j])) {
+                if (j == lo) break;
+            }
+            if (i >= j) break;
+            swap(a, i, j);
+        }
+        swap(a, lo, j);
+        return j;
+    }
+}
