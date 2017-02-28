4ae43a9d39994b1a3c3c44ead7cf96f8a552084c
takoe <IsaevAV27@gmail.com>
2017-03-01 01:50:21 +0300

    Test

--- src/test/java/ru/iav/std/algorithms/p1/w4/task/SolverTest.java
+++ src/test/java/ru/iav/std/algorithms/p1/w4/task/SolverTest.java
@@ -9,7 +9,7 @@ public class SolverTest {
 
     @Test
     public void test() {
-        String inputFileName = "puzzle3x3-unsolvable";
+        String inputFileName = "puzzle10";
         Solver.main(new String[]{inputFileName + ".txt"});
     }
 
