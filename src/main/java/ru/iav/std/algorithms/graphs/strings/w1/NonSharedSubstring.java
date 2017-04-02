3a380104d039f5d85b77c0799ff5d1a64f26408d
takoe <IsaevAV27@gmail.com>
2017-04-02 13:39:43 +0300

    Add starter files for task of week 1 of the course "Algorithms on strings"

---
+++ src/main/java/ru/iav/std/algorithms/graphs/strings/w1/NonSharedSubstring.java
@@ -0,0 +1,31 @@
+package ru.iav.std.algorithms.graphs.strings.w1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class NonSharedSubstring implements Runnable {
+	String solve (String p, String q) {
+		String result = p;
+		return result;
+	}
+
+	public void run () {
+		try {
+			BufferedReader in = new BufferedReader (new InputStreamReader (System.in));
+			String p = in.readLine ();
+			String q = in.readLine ();
+
+			String ans = solve (p, q);
+
+			System.out.println (ans);
+		}
+		catch (Throwable e) {
+			e.printStackTrace ();
+			System.exit (1);
+		}
+	}
+
+	public static void main (String [] args) {
+		new Thread (new NonSharedSubstring ()).start ();
+	}
+}
