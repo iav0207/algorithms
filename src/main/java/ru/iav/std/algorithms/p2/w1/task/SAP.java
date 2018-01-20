9af822198423d71ed1dce9964496f1a72958ccff
Alexander Isaev <IsaevAV27@gmail.com>
2018-01-20 23:15:28 +0300

    Eliminate logic duplication @ SAP

--- src/main/java/ru/iav/std/algorithms/p2/w1/task/SAP.java
+++ src/main/java/ru/iav/std/algorithms/p2/w1/task/SAP.java
@@ -1,5 +1,6 @@
 package ru.iav.std.algorithms.p2.w1.task;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -14,7 +15,7 @@ public class SAP {
     private static final int NO_SUCH_PATH = -1;
 
     private final Digraph g;
-    private final Map<Integer, Map<Integer, SingleRunner>> srCache = new HashMap<>();
+    private final Map<Integer, Map<Integer, Runner>> queryCache = new HashMap<>();
 
     /**
      * Constructor takes a digraph (not necessarily a DAG)
@@ -27,81 +28,49 @@ public class SAP {
      * Length of shortest ancestral path between v and w; -1 if no such path
      */
     public int length(int v, int w) {
-        return singleRunner(v, w).minLength;
+        return runner(v, w).minLength;
     }
 
     /**
      * A common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
      */
     public int ancestor(int v, int w) {
-        return singleRunner(v, w).ancestor;
+        return runner(v, w).ancestor;
     }
 
-    private SingleRunner singleRunner(int v, int w) {
+    private Runner runner(int v, int w) {
         int a = Math.min(v, w), b = Math.max(v, w);
-        srCache.putIfAbsent(a, new HashMap<>());
-        return srCache.get(a).computeIfAbsent(b, ignore -> new SingleRunner(a, b));
-    }
-    
-    private class SingleRunner {
-        int minLength = Integer.MAX_VALUE, ancestor = NO_SUCH_PATH;
-
-        SingleRunner(int v, int w) {
-            check(v, w);
-            if (v == w) {
-                minLength = 0;
-                ancestor = v;
-                return;
-            }
-            BreadthFirstDirectedPaths vBfs = new BreadthFirstDirectedPaths(g, v);
-            BreadthFirstDirectedPaths wBfs = new BreadthFirstDirectedPaths(g, w);
-            Set<Integer> visited = new HashSet<>();
-            Queue<Integer> queue = new Queue<>();
-            queue.enqueue(v);
-            queue.enqueue(w);
-            while (!queue.isEmpty()) {
-                final int c = queue.dequeue();
-                final int vDist = vBfs.distTo(c), wDist = wBfs.distTo(c);
-                visited.add(c);
-                if (vDist > minLength && wDist > minLength) {
-                    continue;
-                }
-                if (vBfs.hasPathTo(c) && wBfs.hasPathTo(c)) {
-                    int length = vDist + wDist;
-                    if (length < minLength) {
-                        ancestor = c;
-                        minLength = length;
-                    }
-                }
-                for (int s : g.adj(c)) if (!visited.contains(s)) queue.enqueue(s);
-            }
-            if (minLength == Integer.MAX_VALUE) minLength = NO_SUCH_PATH;
-        }
+        queryCache.putIfAbsent(a, new HashMap<>());
+        return queryCache.get(a).computeIfAbsent(b, ignore -> new Runner(a, b));
     }
 
     /**
      * Length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
      */
     public int length(Iterable<Integer> v, Iterable<Integer> w) {
-        return multiRunner(v, w).minLength;
+        return newRunner(v, w).minLength;
     }
 
     /**
      * A common ancestor that participates in shortest ancestral path; -1 if no such path
      */
     public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
-        return multiRunner(v, w).ancestor;
+        return newRunner(v, w).ancestor;
     }
 
-    private MultiRunner multiRunner(Iterable<Integer> v, Iterable<Integer> w) {
+    private Runner newRunner(Iterable<Integer> v, Iterable<Integer> w) {
         // mb cache?
-        return new MultiRunner(v, w);
+        return new Runner(v, w);
     }
 
-    private class MultiRunner {
+    private class Runner {
         int minLength = Integer.MAX_VALUE, ancestor = NO_SUCH_PATH;
+        
+        Runner(int v, int w) {
+            this(Collections.singletonList(v), Collections.singletonList(w));
+        }
 
-        MultiRunner(Iterable<Integer> v, Iterable<Integer> w) {
+        Runner(Iterable<Integer> v, Iterable<Integer> w) {
             check(v);
             check(w);
             BreadthFirstDirectedPaths vBfs = new BreadthFirstDirectedPaths(g, v);
@@ -131,14 +100,6 @@ public class SAP {
     }
 
     private void check(Iterable<Integer> vertices) {
-        for (int v : requireNonNull(vertices)) {
-            if (v < 0 || v >= g.V()) {
-                throw new IllegalArgumentException("Vertex of of bounds.");
-            }
-        }
-    }
-
-    private void check(int... vertices) {
         for (int v : requireNonNull(vertices)) {
             if (v < 0 || v >= g.V()) {
                 throw new IllegalArgumentException("Vertex of of bounds.");
